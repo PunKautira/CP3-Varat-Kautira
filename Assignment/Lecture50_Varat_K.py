e961a525a719f718121c85bf944c0cf1f520a47c
Pun <punanda7x@gmail.com>
2019-06-25 22:20:42 +0700

    Lecture50
    Add Lecture50

---
+++ Assignment/Lecture50_Varat_K.py
@@ -0,0 +1,16 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(10,25)
+
+
+def subtractNumber(x,y):
+    print(x-y)
+subtractNumber(10,5)
+
+def multiNumber(x,y):
+    print(x*y)
+multiNumber(3,5)
+
+def divideNumber(x,y):
+    print(int(x/y))
+divideNumber(20,2)
