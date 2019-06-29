9f77f89713ce34dea02472738eeb3d07386d5e5e
Pun <punanda7x@gmail.com>
2019-06-29 19:06:45 +0700

    add new Ex11
    another way to code the loop

--- Assignment/Exercise11_Varat_K.py
+++ Assignment/Exercise11_Varat_K.py
@@ -1,9 +1,9 @@
 number = int(input())
 for x in range(number):
-        print(" "*(number-(x+1)), "*"*(x*2+1))
-        
-        
-        
+        print(" "*(number-(x+1)), "*"*(x*2+1) )
+
+
+
 
 number = int(input())
 for x in range(number):
@@ -11,3 +11,8 @@ for x in range(number):
     for y in range(2*x+1):
         text += "*"
     print(" "*(number-(x+1)), text)
+
+
+number = int(input())
+
+[print(" "*(number-(x+1)), "*"*(x*2+1) ) for x in range(number)]
