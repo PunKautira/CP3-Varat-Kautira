04e080870518552843ad5caafb156282aa717d36
Pun <punanda7x@gmail.com>
2019-09-19 11:50:56 +0700

    Create Lecture105_Varat_K.py

---
+++ Assignment/Lecture105_Varat_K.py
@@ -0,0 +1,34 @@
+class Vehicle:
+    licenseCode = ""
+    serialCode = ""
+    face = ""
+    def turnOnAirConditioner(self):
+        print("Turn On: Air")
+
+class Car(Vehicle):
+    def sayHello(self):
+        print("Hello World")
+
+class PickUp(Vehicle):
+    pass
+
+class Van(Vehicle):
+    pass
+
+class EstateCar(Vehicle):
+    pass
+
+car1 = Car()
+car1.turnOnAirConditioner()
+car1.sayHello()
+
+PickUp1 = PickUp()
+PickUp1.turnOnAirConditioner()
+
+
+Van1 = Van()
+Van1.turnOnAirConditioner()
+
+
+EstateCar1 = EstateCar()
+EstateCar1.turnOnAirConditioner()
